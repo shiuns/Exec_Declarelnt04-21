8c3abd361163df5207353d06d55ad5b886c55b24
Shiun <Shiun@DESKTOP-KPBEE4S>
2022-10-16 18:11:11 +0800

    加入專案檔案。

---
+++ [作業(Exec_Declarelnt 第四題計算)]-21謝孟勳/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _作業_Exec_Declarelnt_第四題計算___21謝孟勳
+{
+    //根據兩個變數大小，視需要對調兩個變數的值
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //老師抱歉..此題看不懂題目QQ
+        }
+    }
+}
